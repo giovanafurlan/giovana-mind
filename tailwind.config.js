ec7161b0b6ed9cb8129c6ca94184b2eefd112a6d
Giovana Furlan <giovananelofurlan@gmail.com>
2025-04-09 23:06:36 -0300

    feat: Add first style

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          100: "#f9c5d1",
+          200: "#f2a6c2",
+        },
+        purple: {
+          100: "#d89ec6",
+          200: "#b490ca",
+          300: "#9795e2",
+        },
+      },
+    },
+  },
+};
