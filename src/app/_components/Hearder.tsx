3cec6fb922c37705a63a7145ae341cf0270c19d8
Giovana Furlan <giovananelofurlan@gmail.com>
2025-04-11 12:53:22 -0300

    fix: Quick fix

--- src/app/_components/Hearder.tsx
+++ src/app/_components/Hearder.tsx
@@ -13,7 +13,7 @@ export default function Header({ display = true }: Props) {
       <div className="flex flex-col items-center gap-4">
         <Image src="/images/logo.png" alt="Logo de brilhos" width={30} height={30} />
         <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-3xl font-bold">Giovana mind</h1>
+          <h1 className="text-3xl font-bold">Giovana's mind</h1>
           <p className="text-gray-600">Conheça mais sobre a vida da Giovana nesse chatbot</p>
         </div>
       </div>
